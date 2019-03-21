46d95764fd6b122b10ce8e160332fe2bf91fd46d
Allan Haldane <allan.haldane@gmail.com>
2019-03-20 20:02:24 -0400

    MAINT: port getAlnSeqs to py3

--- structure/getAlnSeqs.py
+++ structure/getAlnSeqs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 from scipy import *
 import sys, re, argparse
 
@@ -13,7 +13,8 @@ args = parser.parse_args(sys.argv[1:])
 pdbseqs = {}
 pdbfns = []
 with open(args.stkfile) as f:
-    dat = [l.split() for l in f.readlines()[:-1] if (not l.isspace()) and (not l.startswith('#'))]
+    dat = [l.split() for l in f.readlines()[:-1]
+           if (not l.isspace()) and (not l.startswith('#'))]
     for x in dat:
         if x[0] in pdbfns:
             pdbseqs[x[0]] += x[1]
@@ -24,7 +25,8 @@ with open(args.stkfile) as f:
 mode = args.mode
 if args.unalign:
     parts = [l.split('-') for l in args.unalign.split(',')]
-    unalign = concatenate([range(int(p[0]),int(p[1])+1) if len(p) > 1 else [int(p)] for p in parts])
+    unalign = concatenate([range(int(p[0]),int(p[1])+1)
+                           if len(p) > 1 else [int(p)] for p in parts])
 
 for pdbfn in pdbfns:
     seq = pdbseqs[pdbfn]
@@ -42,13 +44,13 @@ for pdbfn in pdbfns:
         seq = "".join(ls)
 
     if mode == 'orig':
-        print head + seq
+        print(head + seq)
     elif mode == 'full':
-        print head + re.sub("[xX.-]", "", seq)
+        print(head + re.sub("[xX.-]", "", seq))
     elif mode == 'aln':
-        print head + re.sub("[a-z.]", "", seq)
+        print(head + re.sub("[a-z.]", "", seq))
     elif mode == 'pdb':
-        print head + re.sub("[.-]", "", seq)
+        print(head + re.sub("[.-]", "", seq))
     elif mode == 'all':
-        print head + seq
+        print(head + seq)
 
--- structure/process.sh
+++ structure/process.sh
@@ -3,30 +3,31 @@
 datadir=$PWD
 hmmfile=$1
 unaligned=$2
+HMMER=../../hmmer-3.1b1-linux-intel-x86_64/binaries/hmmalign
 
 echo "$hmmfile  '$unaligned'" >datasource
 
-#../getPDBseqs.py PDB 100 >seq_log.txt
-#../../hmmer-3.1b1-linux-intel-x86_64/binaries/hmmalign $hmmfile pdbseqs.fa >pdbseqs_aligned.stk
-#../getAlnSeqs.py pdbseqs_aligned.stk orig --showid --unalign $unaligned >rawseqs
-#awk 'gsub(/-/,"-",$2)<32 {print}' rawseqs >pdbseqs_full_ID
-#rm pdbseqs_aligned.stk rawseqs
+../getPDBseqs.py PDB 100 >seq_log.txt
+$HMMER $hmmfile pdbseqs.fa >pdbseqs_aligned.stk
+../getAlnSeqs.py pdbseqs_aligned.stk orig --showid --unalign $unaligned >rawseqs
+awk 'gsub(/-/,"-",$2)<32 {print}' rawseqs >pdbseqs_full_ID
+rm pdbseqs_aligned.stk rawseqs
 
-#sort -u pdbseqs_full_ID -o pdbseqs_full_ID
-#awk '{gsub(/[a-z.]/,"",$2); print }' pdbseqs_full_ID >pdbseqs_ID
-#cut -d ' ' -f 1 pdbseqs_ID >IDs
-#cut -d ' ' -f 2 pdbseqs_ID >pdbseqs
+sort -u pdbseqs_full_ID -o pdbseqs_full_ID
+awk '{gsub(/[a-z.]/,"",$2); print }' pdbseqs_full_ID >pdbseqs_ID
+cut -d ' ' -f 1 pdbseqs_ID >IDs
+cut -d ' ' -f 2 pdbseqs_ID >pdbseqs
 
 #now taken care of in pdb_counts
-#echo "" >>Neffs
-#for weight in none unique m1 0.1 0.4
-#do
-#    echo $weight " " >>Neffs
-#    ../../scripts/phyloWeights.py $weight pdbseqs weights_$weight >>Neffs
-#done
+echo "" >>Neffs
+for weight in none unique m1 0.1 0.4
+do
+    echo $weight " " >>Neffs
+    ../../scripts/phyloWeights.py $weight pdbseqs weights_$weight >>Neffs
+done
 
-#../../scripts/alphaMap.py pdbseqs ../../alignment/map8 >pdbseqs8
-#paste IDs pdbseqs8 >pdbseqs8_ID
+../../scripts/alphaMap.py pdbseqs ../../alignment/map8 >pdbseqs8
+paste IDs pdbseqs8 >pdbseqs8_ID
 
 ../getDists.py PDB
 
